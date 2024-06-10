519caf0f54543524a5347dfb583059efc7190bee
Sean Reid <seanreid.mail@gmail.com>
2024-06-09 22:19:08 -0700

    fix brute force soln and add new dictionaries

--- python/brute_force/lp_brute_force/__init__.py
+++ python/brute_force/lp_brute_force/__init__.py
@@ -11,24 +11,17 @@ class BruteForceSolution(Solution):
         shortest_word: str | None = None
         shortest_word_len: int = 100
         for word in self.dictionary.words:
-            if len(word) < 3:
-                continue
-            contains_all_letters: bool = True
-            letters_in_order: bool = True
-            last_index: int = 0
-            word_copy: str = word
-            for letter in letters:
-                if letter in word_copy:
-                    if last_index > word_copy.index(letter):
-                        letters_in_order = False
-                        break
-                    last_index = word_copy.index(letter)
-                    word_copy = word_copy.replace(letter, "", 1)
-                else:
-                    contains_all_letters = False
-                    break
-            if contains_all_letters and letters_in_order:
+            if self.letters_in_order(letters, word):
                 if len(word) < shortest_word_len:
                     shortest_word = word
                     shortest_word_len = len(word)
         return shortest_word
+
+    def letters_in_order(self, letters: str, word: str) -> bool:
+        pos: int = 0
+        for char in word:
+            if char == letters[pos]:
+                pos += 1
+                if pos == len(letters):
+                    return True
+        return False
